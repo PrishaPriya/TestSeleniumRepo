9aa351ed8d8553169beea14a16159bfcabd8e775
DELL <DELL@DESKTOP-915NSEP>
2023-06-29 23:03:24 +0530

    CreateObject Program

---
+++ src/CreateObject.java
@@ -0,0 +1,45 @@
+
+public class CreateObject {
+	
+
+public class Human {
+		
+		String name;
+		int age;
+		String city;
+		
+		Human h1 = new Human();
+
+	}
+	  class Laptop{
+		 
+		 String Brand;
+		 int Memory;
+		 String color;
+		 
+		 Laptop l1 = new Laptop();
+		 
+	 }
+	 
+	  class SportMan{
+		  
+		  String name;
+		  String sport;
+		  int age;
+		  String country;
+		  
+		  SportMan s1 = new SportMan();
+		  
+		  
+	  }
+	  
+	   class TestCase{
+		   
+		   int testcase_Id;
+		   String scenarios;
+		   String steps;
+		   
+		   TestCase t1 = new TestCase();
+		   
+	   }
+}
