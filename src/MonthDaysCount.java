87175beae087504c32b6da626894e076167cb5aa
DELL <DELL@DESKTOP-915NSEP>
2023-06-30 20:20:53 +0530

    Month Days count

---
+++ src/MonthDaysCount.java
@@ -0,0 +1,96 @@
+
+import java.util.Scanner;
+public class MonthDaysCount {
+	
+	public static void main(String[] args) {
+		
+		int n;
+		System.out.println("Enter a month number");
+		
+		Scanner r = new Scanner(System.in);
+		n=r.nextInt();
+		
+		if(n==1) {
+			
+			System.out.println("Month Jan = 31"); 	
+		}
+		
+		else if(n==2) {
+			
+			System.out.println("Month Feb = 28");
+			
+			}
+		else if(n==3) {
+			
+			System.out.println("Month March = 31");
+		}
+		else if(n==4) {
+			
+			System.out.println("Month April = 30");
+		}
+		
+		else if(n==5) {
+			
+			System.out.println("Month May = 31");
+		}
+		
+		else if(n==6) {
+			
+			System.out.println("Month June = 30");
+		}
+			
+        else if(n==7) {
+			
+			System.out.println("Month July = 31");
+		}
+		
+else if(n==8) {
+			
+			System.out.println("Month Aug = 31");
+		}
+		
+else if(n==9) {
+	
+	System.out.println("Month Sep = 30");
+}
+		
+else if(n==10) {
+	
+	System.out.println("Month Oct = 31");
+}
+		
+else if(n==11) {
+	
+	System.out.println("Month Nov = 30");
+}
+		
+else if(n==12) {
+	
+	System.out.println("Month Dec = 31");
+}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	}
+
+}
