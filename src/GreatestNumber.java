efa652eef90900ea8e3b4cc96502508718ee30b5
DELL <DELL@DESKTOP-915NSEP>
2023-06-30 21:13:34 +0530

    Greater Number

---
+++ src/GreatestNumber.java
@@ -0,0 +1,23 @@
+
+public class GreatestNumber {
+  
+	public static void main(String[] args) {
+		
+		int a = 80, b= 30, c = 140;
+		
+		if(a>b && a>=c) {
+			
+			System.out.println(a);
+		}
+		
+		else if(b>a && b>c) {
+			
+			System.out.println(b);
+		}
+		
+		else {
+			
+			System.out.println(c);
+		}
+	}
+}
