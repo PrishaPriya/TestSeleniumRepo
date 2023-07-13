8c506b36382fe6987677d4d75cf1d65884a528b4
DELL <DELL@DESKTOP-915NSEP>
2023-07-13 09:31:01 +0530

    Variable scope

---
+++ src/StaticProgram2.java
@@ -0,0 +1,21 @@
+
+public class StaticProgram2 {
+	
+	static int i = 10;
+	
+}	
+	
+   class b{
+	   
+	   public static void main(String[] args) {
+		
+		   System.out.println(StaticProgram2.i);
+		   
+		 StaticProgram2 a1= new StaticProgram2();
+		   
+		   System.out.println(a1.i);
+		   
+		   
+	}
+   }
+
