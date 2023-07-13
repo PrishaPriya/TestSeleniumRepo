9327e64e74442b5233467178ed4d562bf2d03a19
DELL <DELL@DESKTOP-915NSEP>
2023-07-13 09:01:59 +0530

    Static Program1

---
+++ src/StaticProgram1.java
@@ -0,0 +1,19 @@
+
+public class StaticProgram1 {
+	
+	static {
+		
+		System.out.println("First block");
+		
+	}
+  
+	 static {
+		 
+		 System.out.println("Second block");
+	 }
+	 
+	 public static void main(String[] args) {
+		
+		 System.out.println("Main method");
+	}
+}
