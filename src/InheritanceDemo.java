1e7bdd39080d4a83bf43127ab7c8bad389ab2fe0
DELL <DELL@DESKTOP-915NSEP>
2023-07-16 20:30:22 +0530

    Extend Keywords

---
+++ src/InheritanceDemo.java
@@ -0,0 +1,33 @@
+
+public class InheritanceDemo {
+	
+	public static void main(String[] args) {
+		
+		child m1 = new child();
+				
+			m1.test2();	
+		    m1.test();
+		
+	}
+}
+   
+	 class parent {
+		
+		public void test() {
+			
+		}
+	}
+	
+	 class child extends parent{
+		  
+		 public void test2() {
+			 
+		 }
+	 
+	 
+		
+	}
+	
+	
+	
+
