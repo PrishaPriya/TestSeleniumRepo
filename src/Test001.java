f24ff70b4daa10f79cf84e08fea29e7ca7c6c758
DELL <DELL@DESKTOP-915NSEP>
2023-07-17 00:26:48 +0530

    Test

---
+++ src/Test001.java
@@ -0,0 +1,14 @@
+
+public class Test001 {
+	
+	
+		private static int x;
+		public static void main(String args[]) {
+		System.out.println(fun());
+		}
+		static int fun() {
+		return ++x;
+		}
+		
+
+}
