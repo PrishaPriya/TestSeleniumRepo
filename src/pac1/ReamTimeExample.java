fd6301f75106d65994eec18df9cb3e92796ace65
DELL <DELL@DESKTOP-915NSEP>
2023-07-18 01:40:35 +0530

    Real time example of Is a relationship.

---
+++ src/pac1/ReamTimeExample.java
@@ -0,0 +1,38 @@
+package pac1;
+
+public class ReamTimeExample {
+  
+	
+}
+ 
+// is a relationship 
+class car{
+	
+	public void breaks() {
+		
+	}
+}
+
+class maruthi{
+	
+	public void horn() {
+		
+	car c1 = new car();
+	c1.breaks();
+	}
+}
+
+// Has a relationship
+
+class parent{
+	
+	public void school() {
+		
+	}
+}
+class child extends parent{
+	
+	public void school() {
+		
+	}
+}
