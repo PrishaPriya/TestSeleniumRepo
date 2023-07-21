f742149c45f2c438ff2b23df840ef2aeb8ca6c20
DELL <DELL@DESKTOP-915NSEP>
2023-07-21 09:33:16 +0530

    All modifiers

---
+++ src/AccessModifiers/AllModifiers.java
@@ -0,0 +1,22 @@
+package AccessModifiers;
+
+public class AllModifiers {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+	}
+
+}
+
+class P{
+	
+}
+
+private class s{
+	
+}
+
+protected class d{
+	
+}
