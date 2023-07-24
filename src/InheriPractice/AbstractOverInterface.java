ee97f01eb466d30004ff961bdae922ab322456e9
DELL <DELL@DESKTOP-915NSEP>
2023-07-24 23:59:48 +0530

    // If we want only method definition then we can create interface.

---
+++ src/InheriPractice/AbstractOverInterface.java
@@ -0,0 +1,23 @@
+package InheriPractice;
+
+public class AbstractOverInterface {
+
+}
+
+// In abstraction we can implements both method (With body and without body.)
+// If we want only method defination then we can create interface.
+
+abstract class India{
+	
+	public abstract void State();
+	
+	public void State1() {
+		
+	}
+}
+
+interface USA{
+	
+	public void State2();
+	public void State3();
+}
