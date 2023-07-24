816891e3aa3836f611b51078081ae6e54c7fe04d
DELL <DELL@DESKTOP-915NSEP>
2023-07-25 00:08:01 +0530

    AccessModifier

---
+++ src/InheriPractice/AccessModifiersInterface.java
@@ -0,0 +1,10 @@
+package InheriPractice;
+
+public interface AccessModifiersInterface {
+	
+	private void G1(); // private method we need to give method body.
+	public void G2();
+	void M3();
+	protected void G4(); // we can create method in interface as a Protected.
+
+}
