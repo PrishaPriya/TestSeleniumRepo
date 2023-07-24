1ef128fb933c47bb23cfb832d5ac9df18833b991
DELL <DELL@DESKTOP-915NSEP>
2023-07-24 21:00:36 +0530

    It is not necessary for abstract class to have abstract method

---
+++ src/InheriPractice/AbstractClassAbstractMethod.java
@@ -0,0 +1,20 @@
+package InheriPractice;
+
+public class AbstractClassAbstractMethod {
+
+}
+
+// It is not necessary for abstract class to have abstract method
+// Abstract class can have both abstract and non abstract method/
+// Because Abstract is a partial abstract.
+
+abstract class Prime{
+	
+	public abstract void Movies();
+	
+	public void Webseries() {
+		
+		System.out.println("Prime");
+		
+	}
+}
