b7b6618946c3a053ffca1135111b32c21c6048df
DELL <DELL@DESKTOP-915NSEP>
2023-07-24 23:43:33 +0530

    we can create constructor in abstract class but no need to create constructor in abstract class.it will not give any error.

---
+++ src/InheriPractice/AbstractClassConstructor.java
@@ -0,0 +1,12 @@
+package InheriPractice;
+
+public abstract class AbstractClassConstructor {
+	
+	public AbstractClassConstructor() {
+		
+	}
+
+}
+
+// we can create constructor in abstract class but no need to create constructor in abstract class.it will not give any error.
+ //  Because abstract class instance is not create. 
