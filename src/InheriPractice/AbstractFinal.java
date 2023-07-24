365d3c8c1f27bf5a44cf4b20d14052782aa0154b
DELL <DELL@DESKTOP-915NSEP>
2023-07-24 23:50:23 +0530

    // Java can not allowed abstract class as a final because abstract - IS a relationship And Final - Has a relationship.

---
+++ src/InheriPractice/AbstractFinal.java
@@ -0,0 +1,10 @@
+package InheriPractice;
+
+public abstract final class AbstractFinal {
+
+}
+
+// No, abstract class can not be final in java.
+
+// Java can not allowed abstract class as a final because abstract - IS a relationshio
+// And Final - Has a relationship.
