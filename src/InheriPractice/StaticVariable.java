b277815be79e4053f34c10fb5df8743ae34a188d
DELL <DELL@DESKTOP-915NSEP>
2023-07-25 20:09:56 +0530

    Static variable

---
+++ src/InheriPractice/StaticVariable.java
@@ -0,0 +1,12 @@
+package InheriPractice;
+
+public class StaticVariable {
+	
+	static int a = 90; // Static method we can not defined by default.
+	public static void main(String[] args) {
+		
+		System.out.println(StaticVariable.a);
+		
+	}
+
+}
