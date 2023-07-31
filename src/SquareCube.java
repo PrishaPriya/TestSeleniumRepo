99229c79a3e275f22854fe4b9defd4f6d572692e
DELL <DELL@DESKTOP-915NSEP>
2023-08-01 01:37:48 +0530

    Square, Cube

---
+++ src/SquareCube.java
@@ -0,0 +1,19 @@
+
+public class SquareCube {
+	
+	public static void main(String[] args) {
+		
+		int n =2;
+		
+		int square = (int)Math.pow(n, 2);
+		int Cube = (int)Math.pow(n, 3);
+		int fouthPower = (int)Math.pow(n, 4);
+		
+		System.out.println("Square of  "+n+" is " +square);
+		System.out.println("Cube of  "+n+" is " +Cube);
+		System.out.println("fouthPower of  "+n+" is " +fouthPower);
+
+
+	}
+
+}
