8bf1be177f8d361d42d52deee4a9d02038272651
DELL <DELL@DESKTOP-915NSEP>
2023-08-05 20:09:09 +0530

    String convert

---
+++ src/StringAllProgram/StringLowerUpperCase.java
@@ -0,0 +1,13 @@
+package StringAllProgram;
+
+public class StringLowerUpperCase {
+	
+	public static void main(String[] args) {
+		
+		String str = "AbC#$dEf";
+		
+		System.out.println(str.toLowerCase());
+		System.out.println(str.toUpperCase());
+	}
+
+}
