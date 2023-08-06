0302efa17fcd161cb732c1013921a1787da5493b
DELL <DELL@DESKTOP-915NSEP>
2023-08-06 19:30:47 +0530

    Constructor

---
+++ src/Constructor/B.java
@@ -0,0 +1,14 @@
+package Constructor;
+
+public class B {
+	
+	B(){    //  This is constructor
+		
+		
+	}
+
+	void B() {    // this is method. because constructor do not have a return type.
+		
+		
+	}
+}
