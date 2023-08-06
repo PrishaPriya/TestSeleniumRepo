2d7f695aaf17c9176f4cd36f30ce64effd762a79
DELL <DELL@DESKTOP-915NSEP>
2023-08-06 18:03:11 +0530

    Constructor as private access modifier and try to create object for object

---
+++ src/Constructor/Constructor3.java
@@ -0,0 +1,18 @@
+package Constructor;
+
+public class Constructor3 {
+	
+	 Constructor3()
+	{
+		
+	}
+	
+}
+
+class ADemo{
+	
+	public static void main(String[] args) {
+		
+		Constructor3 a1 = new Constructor3(); // we make constructor as a private so it will visible within class only.
+	}
+}
