5bb4de7d0702c2cf14e13e9cf5766e9e29d6f3c0
DELL <DELL@DESKTOP-915NSEP>
2023-08-06 20:27:18 +0530

    b12

---
+++ src/Constructor/B12.java
@@ -0,0 +1,20 @@
+package Constructor;
+
+public class B12 {
+	
+	B12(){
+		
+		this(10);
+	}
+ 
+	B12(int i){
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		B12 b1 = new B12(10);
+		
+	System.out.println(b1);
+	}
+}
