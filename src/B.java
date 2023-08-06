f7e689331cc14280a8ab246941aa7b7124099cec
DELL <DELL@DESKTOP-915NSEP>
2023-08-06 19:29:51 +0530

    Constructor

---
+++ src/B.java
@@ -0,0 +1,15 @@
+
+public class B {
+	
+	public static int test1() {
+		
+		return 10;
+		
+		
+	}
+	
+	public static void main(String[] args) {
+	System.out.println("Hello this is test1 method");
+	}
+
+}
