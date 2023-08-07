879073d620f7888124a1a11e3c61c4288be2436b
DELL <DELL@DESKTOP-915NSEP>
2023-08-07 13:22:41 +0530

    Class

---
+++ src/AccessModifiers/ClassWithPrivate.java
@@ -0,0 +1,23 @@
+package AccessModifiers;
+
+public class ClassWithPrivate {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hellow");
+		
+	}
+
+}
+
+class P1{
+	
+	
+}
+
+// protected class P2{ }  
+
+// private class P3{ }
+
+// protected access modifier is only with inheritance will apply.
+// private is visible only within the class.
